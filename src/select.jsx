db10cc30d5bc03d059f89c46701d9410185cf8fb
Dhiraj Indurthy <dhirajindurthy@gmail.com>
2020-04-04 17:17:50 -0500

    Added select change action parameters

--- src/select.jsx
+++ src/select.jsx
@@ -5,6 +5,9 @@ function Select(props) {
     <React.Fragment>
       <label htmlFor={props.id}>{props.label}</label>
       <select onChange={props.onChange} value={props.value}>
+        <option disabled selected value="">
+          Select
+        </option>
         {props.optionData.map((item, index) => (
           <option key={index} value={item.value}>
             {item.label}
